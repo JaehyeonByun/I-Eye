80eca9e2cd9adca9ad9de6fcc72483a6fdc93e77
Jaehyun Byun <143696056+JaehyeonByun@users.noreply.github.com>
2024-11-15 17:30:18 +0900

    feat : Grab

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,27 +7,30 @@ using System.Reflection;
 public class GameManager : MonoBehaviour
 {
     //The GameManager records 18 biometric data points necessary for DSM-5 diagnosis.
-    
-    public static float[] Inattention_a;
-    public static float[] Inattention_b;
-    public static float[] Inattention_c;
-    public static float[] Inattention_d;
-    public static float[] Inattention_e;
-    public static float[] Inattention_f;
-    public static float[] Inattention_g;
-    public static float[] Inattention_i;
-    
-    public static float[] HyperActivity_a;
-    public static float[] HyperActivity_b;
-    public static float[] HyperActivity_c;
-    public static float[] HyperActivity_d;
-    public static float[] HyperActivity_e;
-    public static float[] HyperActivity_f;
-    public static float[] HyperActivity_g;
-    public static float[] HyperActivity_i;
-    
-    
-    public static GameManager instance; 
+
+    // Inattention Parameter
+    public static List<float> Inattention_a = new List<float>();
+    public static List<float> Inattention_b = new List<float>();
+    public static List<float> Inattention_c = new List<float>();
+    public static List<float> Inattention_d = new List<float>();
+    public static List<float> Inattention_e = new List<float>();
+    public static List<float> Inattention_f = new List<float>();
+    public static List<float> Inattention_g = new List<float>();
+    public static List<float> Inattention_i = new List<float>();
+
+    // HyperActivity Parameter
+    public static List<float> HyperActivity_a = new List<float>();
+    public static List<float> HyperActivity_b = new List<float>();
+    public static List<float> HyperActivity_c = new List<float>();
+    public static List<float> HyperActivity_d = new List<float>();
+    public static List<float> HyperActivity_e = new List<float>();
+    public static List<float> HyperActivity_f = new List<float>();
+    public static List<float> HyperActivity_g = new List<float>();
+    public static List<float> HyperActivity_i = new List<float>();
+
+
+    public static GameManager instance;
+
     void Awake()
     {
         if (instance == null)
@@ -40,17 +43,18 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
     public static void MakeInputTensorCSV()
     {
         string timestamp = System.DateTime.Now.ToString("yyyyMMdd_HHmmss");
         string fileName = "ArraysData_" + timestamp + ".csv";
         string filePath = Path.Combine(Application.persistentDataPath, fileName);
-        
+
         string[] arrayPrefixes = { "Inattention", "HyperActivity" };
 
         using (StreamWriter writer = new StreamWriter(filePath))
         {
-   
+
             foreach (string prefix in arrayPrefixes)
             {
                 foreach (FieldInfo field in typeof(GameManager).GetFields(BindingFlags.Public | BindingFlags.Static))
@@ -64,11 +68,15 @@ public class GameManager : MonoBehaviour
                         {
                             writer.WriteLine(value);
                         }
-                        writer.WriteLine(); 
+
+                        writer.WriteLine();
                     }
                 }
             }
         }
 
         Debug.Log("CSV 파일 저장 완료: " + filePath);
-    }}
+    }
+}
+
+
